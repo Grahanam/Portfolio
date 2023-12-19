ecc2eb08f84160cc969a8a9a0a142935bca8773f
Grahanam <lunasuthar5221@gmail.com>
2023-12-19 18:15:26 +0530

    new model added,orbits controls customized

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 import sasukemodel from './models/sasuke.glb?url'
 import sharinganeye from './models/editsharingan.glb?url'
 import waternormals from './static/normals/waternormals.jpeg?url'
-
-
-
+import vfx from './models/vfx.glb?url'
+import fire from './models/fire.glb?url'
 
 import  {Water} from './objects/Water'
 import { Sky } from './objects/Sky';
@@ -22,7 +20,10 @@ let clock = new THREE.Clock()
 let clock2=new THREE.Clock() 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 2000 );
-let renderer
+
+let renderer,controls,vfxmodel,firemodel
+let page1=document.querySelector('.page1')
+let page2=document.querySelector('.page2')
 
 const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
 
@@ -52,8 +53,8 @@ function init(){
     renderer.toneMapping = THREE.ReinhardToneMapping;
     document.body.appendChild( renderer.domElement );
 
-    // let controls = new OrbitControls(camera, renderer.domElement);
-
+    
+    controls = new OrbitControls(camera, renderer.domElement);   
 
     water.rotation.x = -Math.PI / 2;
     water.rotation.z = 0;
@@ -71,7 +72,7 @@ function init(){
     skyUniforms['mieDirectionalG'].value = 0.988;   //0.988  0.8
     
     const parameters = {
-        elevation: 30,
+        elevation: 20,  //30
         azimuth: 190   //175  115
     };
 
@@ -89,15 +90,21 @@ function init(){
     water.material.uniforms['time'].value=10.0
     
     //Add Cylinder Pole to scene
-    const geometry = new THREE.CylinderGeometry( 2, 2, 20, 32 ); 
-    const material = new THREE.MeshBasicMaterial( {color: 0x00000} ); 
+    const geometry = new THREE.CylinderGeometry( 1.8, 1.8, 20, 25 ); 
+    const material = new THREE.MeshStandardMaterial({
+        color: 0x000000, // Base color
+        roughness: 0.1, // Adjust roughness for shininess (lower values are shinier)
+        metalness: 1, // Metalness controls how much the material behaves like meta
+    });
     const cylinder = new THREE.Mesh( geometry, material ); 
     scene.add( cylinder );
 
+    
+
 
     const light = new THREE.HemisphereLight(0xff0000, 0x444444, 1); // Red light
-light.position.set(0, 300, 0);
-scene.add(light);
+    light.position.set(0, 300, 0);
+    scene.add(light);
 
     //Add shadow light to scene
     const shadowLight = new THREE.SpotLight();
@@ -114,10 +121,31 @@ scene.add(light);
     redlight.position.x+=0
     scene.add( redlight );
 
+    //initial position
 
-    camera.position.z = 18;
-    camera.position.y=18
-    camera.rotation.x+=0
+    camera.position.z = 30;
+    camera.position.y=30
+    camera.rotation.x=30
+
+    // camera.position.z = 30;
+    // camera.position.y=25
+    // camera.rotation.x=0
+    // camera.rotation.x=-Math.PI/2
+    // camera.rotation.y=Math.PI/2
+    // camera.rotateX(3)
+    controls.target.set(0,9.9,0)
+    controls.distance=80
+    controls.minDistance=30
+    controls.maxDistance=30
+    controls.minPolarAngle=Math.PI/2
+    controls.maxPolarAngle=Math.PI/2
+    controls.minAzimuthAngle=-0.9
+    controls.maxAzimuthAngle=0.9
+
+    controls.update()
+    
+
+
     let model
     const loader=new GLTFLoader()
 
@@ -126,7 +154,7 @@ scene.add(light);
     // console.log(glft) 
     model=glft.scene
     model.scale.set(12, 12, 12);    //8
-    model.position.y+=9.9
+    model.position.y+=9.8
     // model.rotation.x+=3
     let animation=glft.animations
     // redlight.lookAt(model)
@@ -150,7 +178,38 @@ scene.add(spotLight.target);
     setmodelloading=true
     message.style.display='flex'
     // loaderanimation.remove()
+
+    controls.addEventListener('change',()=>{
+        const maxverticalAngle=Math.PI/3
+        let azimuthangle=controls.getAzimuthalAngle()
+        // console.log(azimuthangle)
+        sharingan.rotation.y=azimuthangle/2
+        firemodel.children[0].rotation.z=azimuthangle
+        vfxmodel.children[0].rotation.z=azimuthangle
+        vfxmodel.rotation.y=azimuthangle/2
+        firemodel.rotation.y=azimuthangle/2
+        if(azimuthangle===0.9){
+            page1.style.display='flex'
+            divopen1=true
+        }else{
+            page1.style.display='none'
+        }
+        if(azimuthangle===-0.9){
+            page2.style.display='flex'
+            divopen2=true
+        }else{
+            page2.style.display='none'
+        }
+        if(azimuthangle<0){
+             sharingan.rotation.x=-azimuthangle/2
+        }else{
+            sharingan.rotation.x=azimuthangle/2
+        }
+        
+    })
 });
+
+
 
 
     //Add Shiringan Eye
@@ -167,28 +226,43 @@ scene.add(spotLight.target);
         
         mixer2 = new THREE.AnimationMixer(sharingan)
         let spinAnim = THREE.AnimationClip.findByName(glft.animations, 'Take 001');
-        // const clip = glft.animations.find((clip) => clip.name === 'Take 001');
-        // console.log(clip)
-        console.log(spinAnim)
+        // console.log(spinAnim)
         eyespin = mixer2.clipAction(spinAnim);
         eyespin.play();
     })
 
+    loader.load(vfx,function(gltf){
+        console.log(gltf)
+          vfxmodel=gltf.scene
+          vfxmodel.scale.set(5,5,5)
+          vfxmodel.position.y=4
+          scene.add(vfxmodel)
+    })
+
+    loader.load(fire,function(gltf){
+        console.log(gltf)
+          firemodel=gltf.scene
+          firemodel.scale.set(0.1,0.1,0.1)
+          firemodel.position.y=9.9
+          scene.add(firemodel)
+    })
+
 }
+
 
 
 //Sharigan Eye following cursor
 function onMouseMove(event){
     //  console.log(event.clientX)
     
-     const mousePosition = {
-        x: (event.clientX / window.innerWidth) * 2 - 1,
-        y: -(event.clientY / window.innerHeight) * 2 + 1,
-    }; 
-    sharingan.rotation.x = -mousePosition.y+0.1;
-    sharingan.rotation.y = mousePosition.x;
+    //  const mousePosition = {
+    //     x: (event.clientX / window.innerWidth) * 2 - 1,
+    //     y: -(event.clientY / window.innerHeight) * 2 + 1,
+    // }; 
+    // sharingan.rotation.x = -mousePosition.y+0.1;
+    // sharingan.rotation.y = mousePosition.x;
 }
-window.addEventListener('mousemove',onMouseMove)
+
 
 window.addEventListener('resize',onWindowResize,false)
 
@@ -201,23 +275,27 @@ function onWindowResize(){
 }
 
 //Click event listener to handle page show/hide,music,loaderanimation hide,message display
-let divopen=true
+let divopen1=false
+page1.addEventListener('click',()=>{
+    if(divopen1){
+        page1.style.display='none'
+        divopen1=false 
+    }
+})
+let divopen2=false
+page2.addEventListener('click',()=>{
+    if(divopen2){
+        page2.style.display='none'
+        divopen2=false 
+    }
+})
 window.addEventListener('click',()=>{
     if(setmodelloading){
         loaderanimation.remove()
         message.style.display='none'
+        setmodelloading=false
         let audio=document.querySelector('#tune')
         audio.play()
-    }
-    let page1=document.querySelector('.page1')
-    
-
-    if(divopen){
-         page1.style.display='none'
-         divopen=false
-    }else{
-         page1.style.display='flex'
-         divopen=true
     }
 })
 
@@ -226,7 +304,6 @@ window.addEventListener('click',()=>{
 //Render animation
 function animate() {
 	requestAnimationFrame( animate );
-
 	renderer.render( scene, camera );
     if (mixer2) {
         mixer2.update(clock2.getDelta());
@@ -239,6 +316,24 @@ function animate() {
     water.material.uniforms['time'].value+=1/60.0
   
 }
+
+// async function transition1(duration){
+//     controls.enableRotate=false
+//     controls.enableZoom=false
+//     gsap.to(camera.position, { duration: duration, ease: "power1.inOut",
+//             x: 0,
+//             y: 18,
+//             z: 18})
+            
+//             // gsap.to(camera.position, { duration: duration, ease: "power1.inOut",
+//             // x: 11,
+//             // y: 1,
+//             // z: 7.6})
+//             // await sleep(15)
+//             controls.enableRotate = true
+//             controls.enableZoom = true
+// }
+
 init()
 animate();
 
